81c44a40c7f76c621ca290e6c08162e4d33de767
elisha111 <elisey.kovalyov@yandex.ru>
2025-12-08 01:24:15 +0300

    #4 состояние компонента

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,27 +1,42 @@
+import { useState } from "react";
 import AddTaskForm from "./AddTaskForm";
 import SearchTaskForm from "./SearchTaskForm";
 import TodoInfo from "./TodoInfo";
 import TodoList from "./TodoList";
 
 const Todo = () => {
-  const tasks = [
+  const [tasks, setTasks] = useState([
     { id: "task-1", title: "Погладить кота", isDone: true },
     { id: "task-2", title: "Погладить кота 2", isDone: false },
     { id: "task-3", title: "Погладить кота 3", isDone: true },
     { id: "task-4", title: "Погладить кота 4", isDone: false },
     { id: "task-5", title: "Погладить кота 5", isDone: true },
-  ];
+  ]);
+
+  const [newTaskTitle, setNewTaskTitle] = useState("");
 
   const deleteAllTasks = () => {
-    console.log("удалить все задачи");
+    const isConfirmed = confirm("Удалить все задачи?");
+
+    if (isConfirmed) {
+      setTasks([]);
+    }
   };
 
   const deleteTasks = (taskId) => {
-    console.log(`удалить задачу id:${taskId}`);
+    setTasks(tasks.filter((task) => task.id !== taskId));
   };
 
   const toggleTaskComplete = (taskId, isDone) => {
-    console.log(`задача id:${taskId} ${isDone ? "выполнена" : "не выполнена"}`);
+    setTasks(
+      tasks.map((task) => {
+        if (task.id === taskId) {
+          return { ...task, isDone };
+        }
+
+        return task;
+      })
+    );
   };
 
   const filterTasks = (query) => {
@@ -29,13 +44,26 @@ const Todo = () => {
   };
 
   const addTask = () => {
-    console.log("задача добавлена");
+    if (newTaskTitle.trim().length > 0) {
+      const newTask = {
+        id: crypto?.randomUUID() ?? Date.now().toString(),
+        title: newTaskTitle,
+        isDone: false,
+      };
+
+      setTasks([...tasks, newTask]);
+      setNewTaskTitle("");
+    }
   };
 
   return (
     <div className="todo">
       <h1 className="todo__title">To Do List</h1>
-      <AddTaskForm addTask={addTask} />
+      <AddTaskForm
+        addTask={addTask}
+        newTaskTitle={newTaskTitle}
+        setNewTaskTitle={setNewTaskTitle}
+      />
       <SearchTaskForm onSearchInput={filterTasks} />
       <TodoInfo
         total={tasks.length}
